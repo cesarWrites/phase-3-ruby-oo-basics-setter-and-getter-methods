f634061bdaf613b7c8baded138c8c3859a4e0d1a
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-14 22:24:45 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    attr_reader :breed
+    def breed=(breed)
+        @breed
+    end
+    attr_writer :breed
+    def breed
+        @breed
+    end
 
 end
+
+Beagle= Dog.new
+Beagle.breed = 'Beagle'
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    attr_reader :name
+    def name=(name)
+        @name
+    end
 
+    attr_writer :name
+    def name
+        @name
+    end
+    attr_reader :job
+    def job=(job)
+        @job
+    end
+    attr_writer :job
+    def job
+        @job
+    end
 end
+
